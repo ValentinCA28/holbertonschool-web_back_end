b102a5e709a31870ed7302f61b31d01ad9f47b4d
ValentinCA28 <12286@holbertonstudents.com>
2026-04-10 16:44:59 +0200

    add concat function with type annotations and main script for testing

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a concat function with type annotations."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result as a string."""
+    return str1 + str2
