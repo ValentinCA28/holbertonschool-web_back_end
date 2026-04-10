6db35163ad9db9313cb6263299ccef3fd2dbb22d
ValentinCA28 <12286@holbertonstudents.com>
2026-04-10 19:34:11 +0200

    feat: add async_comprehension coroutine and main script for random number collection

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that provides an async_comprehension coroutine."""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[int]:
+    """Collect 10 random numbers from async_generator using async comprehension."""
+    return [x async for x in async_generator()]
