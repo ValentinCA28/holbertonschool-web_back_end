376820218e6ba31a2e989d1331717d4aeee524b3
ValentinCA28 <12286@holbertonstudents.com>
2026-04-10 19:23:03 +0200

    feat: add async_generator and main script for generating random numbers

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that provides an async_generator coroutine."""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[int, None]:
+    """Generate 10 random numbers between 0 and 10 with 1 second delay each."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
