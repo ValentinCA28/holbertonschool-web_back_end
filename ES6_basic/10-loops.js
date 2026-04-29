bd1268ed2c7054aee9a4293d225e8f4b10673414
ValentinCA28 <12286@holbertonstudents.com>
2026-04-29 14:16:23 +0200

    task 10 : for / of loops, inverted idx & value

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx in array) {
-    const value = array[idx];
+  for (const [idx, value] of array.entries()) { //
     array[idx] = appendString + value;
   }
   return array;
