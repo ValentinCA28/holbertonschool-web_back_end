8740dcc498a560c145ee275b38983c146d4cb230
ValentinCA28 <12286@holbertonstudents.com>
2026-05-06 14:05:32 +0200

    task 9 : creating map structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+const groceriesList = () => {
+	const map = new Map ();
+	return map
+		.set('Apples', 10)
+		.set('Tomatoes', 10)
+		.set('Pasta', 1)
+		.set('Rice', 1)
+		.set('Bananas', 5)
+}
+export default groceriesList
