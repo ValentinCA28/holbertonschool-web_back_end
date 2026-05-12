d826226d59ed07e5b7a9d043f7f78cca5e06cda7
ValentinCA28 <12286@holbertonstudents.com>
2026-05-12 21:54:17 +0200

    task 1 : process in / out

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Welcome to Holberton School, ${name}`);
-  process.exit();
+    process.stdout.write(`Your name is: ${data.toString()}`);
+});
+
+process.stdin.on('close', () => {
+    process.stdout.write('This important software is now closing\n');
 });
